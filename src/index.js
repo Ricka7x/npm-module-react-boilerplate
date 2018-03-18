0cb478c7a12996cb702017867c94c22d2aef7e1d
ricka7x <ricky.ramirez89@gmail.com>
2018-03-18 16:00:14 -0600

    added support for react and es6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('index.js')
+import React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+  handleClick = () => console.log('clicked')
+
+  render() {
+    return <button onClick={this.handleClick}>hello react</button>
+  }
+}
+
+render(<App />, document.querySelector('#app'))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    port: 3000,
+    open: true
+  }
+}
